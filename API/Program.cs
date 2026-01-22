03982b5fed180804b856d250ced1f4ae2e9d9c74
KristinaK993 <kristina14a@hotmail.com>
2026-01-22 20:56:44 +0100

    added localhost.

--- API/Program.cs
+++ API/Program.cs
@@ -26,7 +26,7 @@ public class Program
             options.AddPolicy("AllowFrontend", policy =>
             {
                 policy
-                    .WithOrigins("http://localhost:5173", "http://localhost:3000")
+                    .WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:8080")
                     .AllowAnyHeader()
                     .AllowAnyMethod();
             });
